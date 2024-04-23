6e0ad4f778b1c6b6528ec482fd9b01147bc74cc1
Yukami <akumahoneko@gmail.com>
2024-04-23 11:13:48 +0700

    Ray of Siphoning particle fix

--- src/main/java/com/yukami/epicironcompat/event/Event.java
+++ src/main/java/com/yukami/epicironcompat/event/Event.java
@@ -4,11 +4,17 @@ import io.redspace.ironsspellbooks.api.events.SpellOnCastEvent;
 import io.redspace.ironsspellbooks.api.events.SpellPreCastEvent;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.spells.CastType;
+import io.redspace.ironsspellbooks.player.ClientMagicData;
+import io.redspace.ironsspellbooks.render.SpellRenderingHelper;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import yesman.epicfight.api.animation.types.StaticAnimation;
+import yesman.epicfight.gameasset.Animations;
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
 import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
 import static yesman.epicfight.gameasset.Animations.*;
@@ -43,7 +49,7 @@ public class Event {
         if (player instanceof ServerPlayer ) {
             playerpatch = EpicFightCapabilities.getEntityPatch(event.getEntity(), ServerPlayerPatch.class);
             if (playerMagicData.getCastType() == CastType.CONTINUOUS) {
-                castAnimation = playerpatch.getAnimator().getLivingAnimation(null, BIPED_DIG);
+                castAnimation = playerpatch.getAnimator().getLivingAnimation(null, BIPED_SPYGLASS_USE);
             }
             else {
                 castAnimation = playerpatch.getAnimator().getLivingAnimation(null, BIPED_JAVELIN_THROW);
@@ -51,6 +57,17 @@ public class Event {
 
             if (castAnimation != null) {
                 playerpatch.playAnimationSynchronized(castAnimation, 0);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void ISbeforeLivingRender(RenderLivingEvent.Pre<? extends LivingEntity, ? extends EntityModel<? extends LivingEntity>> event) {
+        var livingEntity = event.getEntity();
+        if (livingEntity instanceof Player) {
+            var syncedData = ClientMagicData.getSyncedSpellData(livingEntity);
+            if (syncedData.isCasting()) {
+                SpellRenderingHelper.renderSpellHelper(syncedData, livingEntity, event.getPoseStack(), event.getMultiBufferSource(), event.getPartialTick());
             }
         }
     }
--- src/main/java/com/yukami/epicironcompat/mixin/MixinEFISCompat.java
+++ src/main/java/com/yukami/epicironcompat/mixin/MixinEFISCompat.java
@@ -1,38 +1,41 @@
 package com.yukami.epicironcompat.mixin;
 
-import io.redspace.ironsspellbooks.api.spells.CastType;
-import net.minecraft.client.model.EntityModel;
+import com.mojang.blaze3d.vertex.PoseStack;
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.capabilities.magic.SyncedSpellData;
+import io.redspace.ironsspellbooks.render.SpellRenderingHelper;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.client.event.RenderLivingEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import yesman.epicfight.client.events.engine.RenderEngine;
-import io.redspace.ironsspellbooks.player.ClientMagicData;
-import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
-import yesman.epicfight.world.capabilities.EpicFightCapabilities;
-import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
+
+import static com.yukami.epicironcompat.Main.MODID;
+import static io.redspace.ironsspellbooks.render.SpellRenderingHelper.renderRayOfSiphoning;
 import static yesman.epicfight.client.renderer.patched.item.RenderItemBase.renderEngine;
 
 
-@Mixin(value = RenderEngine.Events.class, remap = false)
+@Mixin(value = SpellRenderingHelper.class, remap = false)
 public class MixinEFISCompat {
+    private static final Logger logger = LogManager.getLogger(MODID);
     @Inject(
-            method = {"renderLivingEvent"},
+            method = {"renderSpellHelper"},
             at = @At("HEAD"),
             cancellable = true)
-    private static void EFcancelRenderingLiving(RenderLivingEvent.Pre<? extends LivingEntity, ? extends EntityModel<? extends LivingEntity>> event, final CallbackInfo ci) {
-        LivingEntity livingentity = event.getEntity();
-        CastType ct = ClientMagicData.getCastType();
-        LivingEntityPatch entitypatch;
-        if (renderEngine.hasRendererFor(livingentity)) {
-            entitypatch = EpicFightCapabilities.getEntityPatch(livingentity, LivingEntityPatch.class);
-            if (ClientMagicData.isCasting() && entitypatch instanceof LocalPlayerPatch) {
-                if (ct == CastType.LONG || ct == CastType.CONTINUOUS) {
-                    ci.cancel();
-                }
+    private static void onRenderSpellHelper(SyncedSpellData spellData, LivingEntity castingMob, PoseStack poseStack, MultiBufferSource bufferSource, float partialTicks, final CallbackInfo ci) {
+        try {
+            if (SpellRegistry.RAY_OF_SIPHONING_SPELL.get().getSpellId().equals(spellData.getCastingSpellId())) {
+                logger.info("Spell data: "+spellData);
+                logger.info("Posestack: "+poseStack);
+                renderRayOfSiphoning(castingMob, poseStack, bufferSource, partialTicks);
             }
+        } catch (Exception e) {
+            logger.info("Exception: " + e);
+            throw new RuntimeException(e);
         }
+        ci.cancel();
     }
 }
