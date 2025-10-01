cdf13a0a0623c95401b69e9ea261d924052095a0
Ellet <echo.ellet@gmail.com>
2025-10-01 11:54:47 +0300

    feat: WIP port to NeoForge 1.21.1
    What needs to be done before merge:
    
    - Migrate to NeoForge networking https://docs.neoforged.net/docs/networking/
    - Fix all newly added TODOs in code
    - More in-game testing

--- src/main/java/com/yukami/efiscompat/EpicFightIronCompat.java
+++ src/main/java/com/yukami/efiscompat/EpicFightIronCompat.java
@@ -1,39 +1,29 @@
 package com.yukami.efiscompat;
 
-import com.mojang.logging.LogUtils;
 import com.yukami.efiscompat.animation.Animation;
 import com.yukami.efiscompat.config.CommonConfig;
 import com.yukami.efiscompat.data.SpellAnimationLoader;
 import com.yukami.efiscompat.network.Networking;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.AddReloadListenerEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod("efiscompat")
+@Mod(EpicFightIronCompat.MODID)
 public class EpicFightIronCompat {
-    public static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "efiscompat";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-    public EpicFightIronCompat() {
-        // Retrieve the mod loading context
-        FMLJavaModLoadingContext context = FMLJavaModLoadingContext.get();
-        IEventBus bus = context.getModEventBus();
-
-        // Register setup method
-        bus.addListener(this::commonSetup);
-
-        // Register the configuration file
-        context.getModEventBus().register(this);
-        context.registerConfig(ModConfig.Type.COMMON, CommonConfig.CONFIG, "efiscompat.toml");
-
-        // Add other event listeners
-        bus.addListener(Animation::registerAnimations);
-        MinecraftForge.EVENT_BUS.addListener(this::onResourceReload);
+    public EpicFightIronCompat(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        modContainer.registerConfig(ModConfig.Type.COMMON, CommonConfig.CONFIG, "efiscompat.toml");
+        modEventBus.addListener(Animation::registerAnimations);
+        NeoForge.EVENT_BUS.addListener(this::onResourceReload);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
--- src/main/java/com/yukami/efiscompat/effect/RenderSiphonRay.java
+++ src/main/java/com/yukami/efiscompat/effect/RenderSiphonRay.java
@@ -11,13 +11,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.neoforge.client.event.RenderPlayerEvent;
 
-@Mod.EventBusSubscriber(modid = EpicFightIronCompat.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = EpicFightIronCompat.MODID, value = Dist.CLIENT)
 public class RenderSiphonRay {
     @SubscribeEvent
     public static void renderRay(RenderLevelStageEvent event) {
@@ -32,15 +32,18 @@ public class RenderSiphonRay {
         if (syncedData.isCasting() && SpellRegistry.RAY_OF_SIPHONING_SPELL.get().getSpellId().equals(syncedSpellData.getCastingSpellId())) {
             poseStack.pushPose();
             poseStack.translate(0, -1.5f, 0);
-            SpellRenderingHelper.renderSpellHelper(syncedSpellData, livingEntity, poseStack, buffer, event.getPartialTick());
+            // TODO: (1.21.1 PORT) Double-check that this behaves the same as event.getPartialTick() in Forge 1.20.1.
+            SpellRenderingHelper.renderSpellHelper(syncedSpellData, livingEntity, poseStack, buffer, event.getPartialTick().getGameTimeDeltaPartialTick(true));
             poseStack.popPose();
         }
     }
-        @SubscribeEvent
-        public static void renderRayTPS(RenderPlayerEvent event){
-            if (event.getEntity() instanceof Player) {
-                SyncedSpellData syncedSpellData = ClientMagicData.getSyncedSpellData(event.getEntity());
-                SpellRenderingHelper.renderSpellHelper(syncedSpellData, event.getEntity(), event.getPoseStack(),event.getMultiBufferSource(), event.getPartialTick());
-            }
+
+    @SubscribeEvent
+    // TODO: (1.21.1 PORT) Should this be RenderPlayerEvent.Post or RenderPlayerEvent.Pre to behave in the same way as in Forge 1.20.1?
+    public static void renderRayTPS(RenderPlayerEvent.Post event) {
+        if (event.getEntity() instanceof Player) {
+            SyncedSpellData syncedSpellData = ClientMagicData.getSyncedSpellData(event.getEntity());
+            SpellRenderingHelper.renderSpellHelper(syncedSpellData, event.getEntity(), event.getPoseStack(), event.getMultiBufferSource(), event.getPartialTick());
         }
     }
+}
